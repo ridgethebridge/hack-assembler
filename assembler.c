79aa2f43a005be13addd124f899dffb9a6608d7b
tombour <ridgerouzier@gmail.com>
2025-03-22 14:17:56 -0400

    output file now has name or iput, suffixed with .hack

--- assembler.c
+++ assembler.c
@@ -8,6 +8,16 @@
 #define NOFILE_GIVEN 1
 #define OPEN_FAILURE 2
 
+int dot_loc(char *file) {
+	int i = 0;
+	while(file[i]) {
+		if(file[i] == '.') {
+			return i;
+		}
+		++i;
+	}
+	return -1;
+}
 
 int main(int argc, char** argv) {
 	if(argc < 2) {
@@ -21,7 +31,22 @@ int main(int argc, char** argv) {
 	char ins[128];
 	char d_ins[17];
 	d_ins[16] = '\0';
-	FILE *exe = fopen("bin.hack","w");
+	int ext_begin = dot_loc(argv[1]);
+	char *out_file;
+	if(ext_begin == -1) {
+		size_t input_len = strlen(argv[1]);
+		out_file = calloc(input_len+5,sizeof(char));
+		memcpy(out_file,argv[1],input_len);
+		strcpy(out_file+input_len,".hack");
+	}
+	else {
+		out_file = calloc(ext_begin+5,sizeof(char));
+		memcpy(out_file,argv[1],ext_begin);
+		strcpy(out_file+ext_begin,".hack");
+	}
+
+
+	FILE *exe = fopen(out_file,"w");
 	while(has_next()) {
 		read_instruction(ins);
 		switch(command_type(ins)) {
--- bin.tst
+++
@@ -1,4 +0,0 @@
-load bin.hack;
-repeat {
-ticktock;
-}
